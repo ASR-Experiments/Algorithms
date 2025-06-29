2846690daf9642b51331316897530f1cd2f45966
Arvind Singh Rawat <arvind160102261@gmail.com>
2025-06-29 14:41:11 +0530

    Add sub-filter project
    Signed-off-by: Arvind Singh Rawat <arvind160102261@gmail.com>

---
+++ src/filter/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("org.sonarqube") version "6.2.0.5505"
+}
+
+group = "org.asr.example"
+version = "0.0.1-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "asr-experiments_Algorithms_Filter")
+        property("sonar.organization", "asr-experiments")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
